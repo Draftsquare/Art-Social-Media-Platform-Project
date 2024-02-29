d5fb90893638b7702c431963de400e68fc3a5bfc
Draftsquare <Draftition@gmail.com>
2024-02-28 20:02:48 -0500

    Added basic code to begin the creation of ArtWave

--- index.php
+++ index.php
@@ -1,10 +1,48 @@
-<!DOCTYPE html>
+<?php
 
+    // 1. Create a database connection
+    // 2. Perform a database query
+    // 3. Use the returned data from the database
+    // 4. Release the return data
+   //  5. Close the database connection
+  
+   $databaseConnection = mysqli_connect( "localhost", "root", "", "ArtWave" );
+
+   if ( mysqli_connect_errno() ) {
+        exit( "Database connection failed" );
+   }
+
+   echo ( "Connection to the database is successful" );
+  
+  
+   if( $databaseConnection ) {
+    mysqli_close( $databaseConnection );
+    echo( "Connection closed");
+   } else{
+    echo( "There was no connection in the first place" );
+   }
+
+
+
+?>
+
+<!DOCTYPE html>
 <html>
     <head>
-        <title>Test PHP page temp</title>
+        <title>ArtWave</title>
+        <link href="css/ArtWave.css" rel="stylesheet">
     </head>
     <body>
-        <?php echo ( phpinfo() ); ?>
+        <h1>Welcome to ArtWave!</h1>
+        <form>
+            <textarea></textarea>
+            <input type="submit" value="Post">
+        </form>
     </body>
 </html>
+
+<?php
+ if( $databaseConnection ) {
+    mysqli_close( $databaseConnection );
+ }
+?>
