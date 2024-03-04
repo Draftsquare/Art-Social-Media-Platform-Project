e67c50d50a68bc65a6bc931caf61395bce56d1fb
Draftsquare <Draftition@gmail.com>
2024-03-04 14:20:52 -0500

    Coding progress update, added working SQL.

--- index.php
+++ index.php
@@ -6,24 +6,50 @@
     // 4. Release the return data
    //  5. Close the database connection
   
+
+
    $databaseConnection = mysqli_connect( "localhost", "root", "", "ArtWave" );
 
    if ( mysqli_connect_errno() ) {
         exit( "Database connection failed" );
+   } else {
+        // echo( "Success!" );
    }
 
-   echo ( "Connection to the database is successful" );
+   // echo ( "Connection to the database is successful" );
   
-  
-   if( $databaseConnection ) {
-    mysqli_close( $databaseConnection );
-    echo( "Connection closed");
-   } else{
-    echo( "There was no connection in the first place" );
+   // Temporary user id
+   $userId = 1;
+   
+
+   if( $_SERVER[ 'REQUEST_METHOD'] == 'POST' && isset( $_POST['postButtonClicked'] ) ) {
+        $postContent = mysqli_real_escape_string ( $databaseConnection, $_POST['postContent'] );
+
+        $sql = "INSERT INTO posts (";
+        $sql .= "postContent, userid)";
+        $sql .= " VALUES ( ";
+        $sql .= "'" . $postContent . "', ";
+        $sql .= "'". $userId . "'";
+        $sql .= ")";
+
+       /*() echo($sql);
+        exit(); */
+
+        $postInsertionSuccessful = mysqli_query( $databaseConnection, $sql );
+
+       if( $postInsertionSuccessful ) {
+            echo("Post Sueccessfully inserted");
+       } else {
+        echo( mysqli_error ($databaseConnection) );
+
+        if( $databaseConnection ) {
+            mysqli_close( $databaseConnection );
+         }
+
+        exit();
+       }
    }
-
-
-
+   
 ?>
 
 <!DOCTYPE html>
@@ -34,15 +60,15 @@
     </head>
     <body>
         <h1>Welcome to ArtWave!</h1>
-        <form>
-            <textarea></textarea>
-            <input type="submit" value="Post">
+        <form action="index.php" method="post">
+            <textarea name="postContent"></textarea>
+            <input type="submit" value="Post" name="postButtonClicked">
         </form>
     </body>
 </html>
 
 <?php
- if( $databaseConnection ) {
+    if( $databaseConnection ) {
     mysqli_close( $databaseConnection );
  }
 ?>
