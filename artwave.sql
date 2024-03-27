10f378313675a19bb16869ce2631006240276ae5
Draftsquare <Draftition@gmail.com>
2024-03-27 14:43:17 -0400

    Added screenshots, small amounts of code

--- artwave.sql
+++ artwave.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 14, 2024 at 05:57 PM
+-- Generation Time: Mar 27, 2024 at 07:42 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `likes`
+--
+
+CREATE TABLE `likes` (
+  `id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `posts`
 --
 
@@ -39,9 +52,34 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `postContent`, `date`, `userid`) VALUES
-(25, 'I\'m ', '2024-03-07 11:40:54', 1),
-(32, 'What\'s up', '2024-03-12 12:27:35', 2),
-(33, '<h1>I\'m a super star!</h1>', '2024-03-12 12:32:10', 2);
+(34, 'hola', '2024-03-14 13:45:11', 15),
+(35, 'hola', '2024-03-14 13:47:01', 16),
+(36, 'hola', '2024-03-14 13:47:02', 15),
+(37, 'hola', '2024-03-18 12:34:17', 15),
+(38, 'hola', '2024-03-18 12:39:31', 15),
+(39, 'hola', '2024-03-18 12:39:51', 15),
+(40, 'hola', '2024-03-18 12:41:23', 15),
+(41, 'hola', '2024-03-18 12:42:42', 15),
+(42, 'hola', '2024-03-18 12:43:10', 15),
+(43, 'hola', '2024-03-18 13:02:29', 15),
+(44, 'hola', '2024-03-18 14:06:45', 15),
+(45, 'hola', '2024-03-18 14:07:10', 15),
+(46, 'hola', '2024-03-18 14:28:33', 15),
+(47, 'blah', '2024-03-27 14:35:56', 15);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `replies`
+--
+
+CREATE TABLE `replies` (
+  `id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `reply_content` text NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -60,22 +98,35 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `nickname`, `hashedPassword`) VALUES
-(1, 'Jessie', ''),
-(2, 'James', ''),
-(9, 'Alex', '$2y$10$cHbngf74afvkyW2.SH6kYe/OBfeGeBqTDvrVcNHMINmMMaMP3iFKC'),
-(10, 'Alex', '$2y$10$cHbngf74afvkyW2.SH6kYe/OBfeGeBqTDvrVcNHMINmMMaMP3iFKC'),
-(11, 'Alex', '$2y$10$UMwlJ6llvxJOq1dC809TmONDTsYTELFuCBNAylnJShkTk7TFRm0Lq'),
-(12, 'Alex', '$2y$10$UMwlJ6llvxJOq1dC809TmONDTsYTELFuCBNAylnJShkTk7TFRm0Lq');
+(15, 'Alex', '$2y$10$qycYtvPrF9fKyrdQNRP.FOquAaZwAZ/MsqzqQ3nQOc4HOePfgu07C'),
+(16, 'NotAlex', '$2y$10$MeazUdZhnzy/egbXhzTjXuGpESLlnug.JGD1GRbKiEvkb4n5CHAAe');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_like` (`post_id`,`user_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Indexes for table `posts`
 --
 ALTER TABLE `posts`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_userid` (`userid`);
+
+--
+-- Indexes for table `replies`
+--
+ALTER TABLE `replies`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `post_id` (`post_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `users`
@@ -88,16 +139,52 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+
+--
+-- AUTO_INCREMENT for table `replies`
+--
+ALTER TABLE `replies`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `likes`
+--
+ALTER TABLE `likes`
+  ADD CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`),
+  ADD CONSTRAINT `likes_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `fk_userid` FOREIGN KEY (`userid`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `replies`
+--
+ALTER TABLE `replies`
+  ADD CONSTRAINT `replies_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`),
+  ADD CONSTRAINT `replies_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
